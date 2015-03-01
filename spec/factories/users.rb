59c3a4389c3219716b6970c2d3f213ac95d3e8d4
Pavel Makarov <metus.violarium@gmail.com>
2015-03-01 15:27:08 +0300

    Factory girl added.

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  # Classic user.
+  factory :user do
+    email 'example@email.com'
+    password 'password'
+  end
+
+  # User with corrupted password (somehow).
+  factory :corrupt_password_user, class: User do
+    email 'example@email.com'
+    encrypted_password 'corrupted'
+  end
+end
