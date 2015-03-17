104e8d2f476990ca6bbc5b9427a7c138eeba15f1
Pavel Makarov <metus.violarium@gmail.com>
2015-03-17 22:31:47 +0300

    Gems updated. Assets made quiet in development. Current user does not call query many times for nil user.

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # Don't mess server output with assets
+  gem 'quiet_assets'
 end
 
 # Gems for testing
--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,26 +3,60 @@ module Authenticable
 
   private
 
+
   # Sign in user.
+  #
+  # @param user [User]
   def sign_in_user(user)
-    @current_user = user
+    clear_current_user
+    set_current_user { user }
     session[:user_id] = user.id.to_s
   end
 
+
   # Sign out user.
   def sign_out
+    clear_current_user
     session[:user_id] = nil
   end
 
+
   # Get current signed in user.
+  #
+  # @return [User] return current user or nil
   def current_user
-    @current_user ||= User.where(id: session[:user_id]).first
+    set_current_user { User.where(id: session[:user_id]).first }
   end
 
+
+  # Set user as current if it's not already set.
+  #
+  # @yield block which will return user
+  #
+  # @return [User]
+  def set_current_user(&block)
+    unless @current_user_initialized
+      @current_user = block.call
+      @current_user_initialized = true
+    end
+    @current_user
+  end
+
+
+  # Clear current user.
+  def clear_current_user
+    @current_user = nil
+    @current_user_initialized = false
+  end
+
+
   # Check if user is signed in.
+  #
+  # @return [Boolean]
   def user_signed_in?
     !current_user.nil?
   end
+
 
   # Filter to require sign in
   def require_sign_in
@@ -31,6 +65,7 @@ module Authenticable
       redirect_to sign_in_url
     end
   end
+
 
   included do
     helper_method :current_user, :user_signed_in?
