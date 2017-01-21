b9ceb09c2cafb7c62836905f41161e8ef7eb0ffa
Pavel Makarov <metus.violarium@gmail.com>
2017-01-21 20:05:34 +0300

    Test webpush json.

--- app/assets/javascripts/serviceworker.js
+++ app/assets/javascripts/serviceworker.js
@@ -1,6 +1,24 @@
 // The serviceworker context can respond to 'push' events and trigger
 // notifications on the registration property
 self.addEventListener("push", function (event) {
-    var title = (event.data && event.data.text()) || '';
-    event.waitUntil(self.registration.showNotification(title));
+    if (!(self.Notification && self.notification.permission === 'granted')) {
+        return;
+    }
+
+    var data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    var title = data.title;
+    var message = data.message;
+    var link = data.link;
+
+    var notification = new Notification(title, {body: message});
+
+    notification.addEventListener('click', function() {
+        if (clients.openWindow) {
+            clients.openWindow(link);
+        }
+    });
 });
